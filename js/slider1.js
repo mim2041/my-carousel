d618286a4d4c5e04cf95bfc54ffdf547f7e82d45
mim2041 <suraiyamim338@gmail.com>
2023-03-17 22:36:49 +0600

    another slider website made

---
+++ js/slider1.js
@@ -0,0 +1,21 @@
+let images = [
+    "images/pic-1.jpg",
+    "images/pic-2.jpg",
+    "images/pic-3.jpg",
+    "images/pic-4.jpg",
+    "images/pic-5.jpg",
+    "images/pic-6.jpg",
+    "images/pic-7.jpg",
+    "images/pic-8.jpg",
+    "images/pic-9.jpg" 
+];
+let imgIndex = 0;
+const sliderImg = document.getElementById('slider-img');
+setInterval ( () => {
+    if(imgIndex === images.length){
+        imgIndex = 0;
+    }
+    const url = images[imgIndex];
+    sliderImg.setAttribute('src', url);
+    imgIndex++;
+},1500)

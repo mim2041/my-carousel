8ae3c36ae0473759786958d645d5d8b60eb02022
mim2041 <suraiyamim338@gmail.com>
2023-04-23 22:32:50 +0600

    console.table

---
+++ consoleTable.js
@@ -0,0 +1,9 @@
+// console.table is a method in the JavaScript console that allows to display tabular data in a more organized and readable format. It takes ana array or an object as an argument and displays it as a table in the console.
+
+const data = [
+    {name: 'John', age: 30, occupation: 'Developer'},
+    {name: 'Mim', age: 20, occupation: 'Student'},
+    {name: 'mitu', age: 25, occupation: 'Teacher'}
+];
+
+console.table(data);
